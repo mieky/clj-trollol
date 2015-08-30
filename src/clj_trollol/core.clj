f4952cb34f48833484846b8602dfb7d6ee2b4852
Mike Arvela <mike.arvela@futurice.com>
2015-08-30 13:58:32 +0300

    Update example, filter a bit

--- src/clj_trollol/core.clj
+++ src/clj_trollol/core.clj
@@ -5,17 +5,30 @@
 (defn read-json-file [name]
     (json/read-str (slurp name) :key-fn keyword))
 
+(defn get-list-name
+  [boardId]
+  )
+
+(defn filter-props
+  "Get only those properties we need"
+  [cards]
+  (map
+    #(assoc {} :idList (:idList %) :idLabels (:idLabels %))
+    cards))
+
 (defn get-list-ids
   "Get number of candidates indexes by list id"
   [cards]
-  (map #(assoc {} (key %) (count (second %)))
-      (group-by :idList cards)))
+  (map
+    #(assoc {} (key %) (count (second %)))
+    (group-by :idList cards)))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Read cards and output a JSON summary of their lists"
   [& args]
   (json/pprint (->
       (read-json-file "cards.debug.json")
+      (filter-props)
       (get-list-ids)
       (json/write-str)
       )))

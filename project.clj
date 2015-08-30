5883df22a7789c18e58cb68a377a0ae350a16a41
Mike Arvela <mike.arvela@futurice.com>
2015-08-30 12:28:03 +0300

    Read some JSON from a file

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [
+    [org.clojure/clojure "1.6.0"]
+    [org.clojure/data.json "0.2.6"]
+    [clj-http "2.0.0"]
+    [environ "0.5.0"]]
   :main ^:skip-aot clj-trollol.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/clj_trollol/core.clj
+++ src/clj_trollol/core.clj
@@ -1,7 +1,11 @@
 (ns clj-trollol.core
+  (:require [clojure.data.json :as json])
   (:gen-class))
+
+(defn read-json-file [name]
+    (json/read-str (slurp name)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (json/pprint (read-json-file "lists.debug.json")))

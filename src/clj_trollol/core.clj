c6eee6406ffd4f64e8169065c329648c04b7ee7c
Mike Arvela <mike.arvela@futurice.com>
2015-09-05 16:48:53 +0300

    Fetch cards from Trello + simplify!

--- src/clj_trollol/core.clj
+++ src/clj_trollol/core.clj
@@ -1,47 +1,61 @@
 (ns clj-trollol.core
   (:require [clojure.data.json :as json])
+  (:require [clojure.set :as set])
+  (:require [clj-http.client :as client])
   (:gen-class))
-
-(defn read-json-file [name]
-    (json/read-str (slurp name) :key-fn keyword))
 
 (defn read-secrets
   "Read API secrets from environment variables"
   []
   {:key   (System/getenv "KEY")
-   :token (System/getenv "TOKEN")})
+   :token (System/getenv "TOKEN")
+   :board (System/getenv "BOARD")})
 
-(defn filter-props
-  "Get only those properties we need"
-  [cards]
-  (map
-    #(assoc {} :idList (:idList %) :idLabels (:idLabels %))
-    cards))
+(defn work-locally? []
+  (some? (System/getenv "LOCAL")))
 
-(defn get-list-ids
-  "Get number of candidates indexes by list id"
-  [cards]
-  (map
-    #(assoc {} (key %) (count (second %)))
-    (group-by :idList cards)))
+(defn parse-json [input]
+  (json/read-str input :key-fn keyword))
+
+(defn read-json-file [name]
+  (parse-json (slurp name)))
+
+(defn read-list-names []
+  (json/read-str (slurp "config.json")))
+
+(defn convert-ids-to-names
+  [keys-with-ids]
+  (set/rename-keys keys-with-ids (read-list-names)))
+
+(defn get-frequencies [cards]
+  (frequencies (map #(:idList %) cards)))
+
+(defn read-from-trello
+  "Go fetch the cards from Trello API"
+  [secrets]
+  (let [url (str "https://api.trello.com/1/boards/" (:board secrets)
+                 "/cards?key=" (:key secrets)
+                 "&token=" (:token secrets))]
+    (parse-json (:body (client/get url {:accept :json})))))
 
 (defn print-usage "" []
-  (println "Please pass KEY and TOKEN as environment variables."))
+  (println "Please pass environment variables KEY, TOKEN, BOARD."))
 
 (defn fetch-cards
   "Fetch cards"
-  []
-  (json/pprint (->
-      (read-json-file "cards.debug.json")
-      (filter-props)
-      (get-list-ids)
-      (json/write-str)
-      )))
-
+  [secrets]
+  (json/pprint (-> (if (work-locally?)
+                     (parse-json (slurp "cards.debug.json"))
+                     (read-from-trello secrets))
+                   (get-frequencies)
+                   (convert-ids-to-names)
+                   (json/write-str))))
+      
 (defn -main
   "Read cards and output a JSON summary of their lists"
   [& args]
   (let [secrets (read-secrets)]
       (if (or (nil? (:key secrets)) (nil? (:token secrets)))
         (print-usage)
-        (fetch-cards))))
+        (fetch-cards secrets))))
+
